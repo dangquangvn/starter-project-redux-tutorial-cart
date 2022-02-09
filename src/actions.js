2b4901ea9e87432bffe08d7dd23ce7621e31c946
quangTD <tsquang398@gmail.com>
2022-02-09 23:20:15 +0700

    :sparkles: [26 REDUX CART] 393 handle "REMOVE" action

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 export const DECREASE = "DECREASE";
 export const INCREASE = "INCREASE";
+export const CHANGE_AMOUNT = "CHANGE_AMOUNT";
 export const REMOVE = "REMOVE";
 export const CLEAR_CART = "CLEAR_CART";
 export const GET_TOTAL = "GET_TOTAL";
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import { CLEAR_CART, DECREASE, INCREASE } from "./actions";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./actions";
 
 //= method 1: using if statement
 // function reducer(state, action) {
@@ -25,6 +25,17 @@ function reducer(state, { type, payload }) {
   switch (type) {
     case CLEAR_CART:
       return { ...state, cart: [], total: 0, amount: 0 };
+    case DECREASE:
+      console.log("decrease reducer");
+      return { ...state };
+    case INCREASE:
+      console.log("increase reducer");
+      return { ...state };
+    case REMOVE: {
+      console.log("remove reducer");
+      const tempCart = state.cart.filter((item) => item.id !== payload.id);
+      return { ...state, cart: tempCart };
+    }
     default:
       return { ...state };
   }
