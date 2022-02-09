494266cb6e01407099af498f8ea29226088c5953
quangTD <tsquang398@gmail.com>
2022-02-09 16:46:19 +0700

    :sparkles: [26 REDUX CART] 383. Actions as Variables

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import cartItems from "./cart-items";
 // return updated or old state
 
 import { createStore } from "redux";
+import { CLEAR_CART, DECREASE, INCREASE } from "./actions";
 
 // store.getState()
 
@@ -23,11 +24,36 @@ const initialStore = {
 // reducer
 function reducer(state, action) {
   console.log({ state, action });
+  if (action.type === DECREASE) {
+    // mutate
+    // state.count = state.count - 1
+    // immutate
+    return { ...state, count: state.count - 1 };
+  }
+  if (action.type === INCREASE) {
+    return { ...state, count: state.count + 1 };
+  }
+  if (action.type === "RESET") {
+    return { ...state, count: 0 };
+  }
   //& always need return state
   // if nothing return, it will return undefined state
   return state;
 }
 const store = createStore(reducer, initialStore);
+console.log("store.getState", store.getState());
+
+// dispatch method - send actions to the store
+// actions (objects) - MUST HAVE TYPE PROPERTY - what kind of action
+// DONOT MUTATE THE STATE - redux build on IMMUTABILITY (copy)
+// copy 5 times
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: CLEAR_CART });
 console.log("store.getState", store.getState());
 
 function App() {
